f75b3b0b04d5e044a2eacf41bac957072806a6e1
prashant chavan <prashantchavan312199@gmail.com>
2022-03-16 01:36:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+   <div className='App'>
+<h1> Build A hero </h1>
+<lable> Name</lable>
+<input type ='text' style={{margin:10}} />
+<lable> Age</lable>
+<input type ='number'style={{margin:10}}/>
+<lable> Height</lable>
+<input type ='text'style={{margin:10}}/>
+<lable> Superpower</lable>
+<input type ='text'style={{margin:10}}/>
+
     </div>
   );
 }
